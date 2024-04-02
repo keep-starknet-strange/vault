d108601f06da3e5475ecb98b04209a023b3c4d17
Francesco Ceccon <francesco@ceccon.me>
2024-04-02 21:19:55 +0200

    Add historical USDC balance indexer

--- infra/indexers/init.sql
+++ infra/indexers/init.sql
@@ -11,3 +11,12 @@ create table transfer_usdc(
     created_at timestamp default current_timestamp,
     _cursor bigint
 );
+
+create table balance_usdc(
+    network text,
+    block_number bigint,
+    block_timestamp timestamp,
+    address text,
+    balance text,
+    _cursor bigint
+);
