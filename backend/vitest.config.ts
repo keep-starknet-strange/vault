2d8a746695b896ce157b04c98f2d11a68253e764
LucasLvy <lucaslevy10@gmail.com>
2024-04-11 16:08:01 +0200

    try coverage

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
-  test: { hookTimeout: 50000 },
+  test: {
+    hookTimeout: 50000,
+    coverage: { provider: 'istanbul', reporter: ['text', 'json-summary', 'json'] },
+  },
 });
