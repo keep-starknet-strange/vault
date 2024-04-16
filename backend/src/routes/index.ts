ae3ab8e7949949c4828d8013d0cef3dab25fca43
Gift-Naomi <emmanuelgiftnaomi@gmail.com>
2024-04-16 17:22:02 +0100

    refac(backend): declare getLimitRoute

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,16 +1,17 @@
-import type { Database } from '@/db/drizzle';
-import { sql } from 'drizzle-orm';
-import type { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import type { Database } from "@/db/drizzle";
+import { sql } from "drizzle-orm";
+import type { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 
 import { getClaimRoute } from './claim';
 import { getGenerateClaimLinkRoute } from './generateClaimLink';
-import { getBalanceRoute } from './getBalance';
 import { getCurrentExpenseRoute } from './getCurrentExpense';
-import { getHistoricalBalanceRoute } from './getHistoricalBalance';
 import { getOtp } from './getOtp';
 import { getTransactionHistory } from './getTransactionHistory';
 import { getRegisterRoute } from './register';
 import { verifyOtp } from './verifyOtp';
+import { getBalanceRoute } from "./getBalance";
+import { getHistoricalBalanceRoute } from "./getHistoricalBalance";
+import { getLimitRoute } from "./getLimit";
 
 export const addressRegex = /^0x0[0-9a-fA-F]{63}$/;
 
@@ -25,12 +26,16 @@ export function declareRoutes(fastify: FastifyInstance) {
   getHistoricalBalanceRoute(fastify);
   getGenerateClaimLinkRoute(fastify);
   getClaimRoute(fastify);
+  getLimitRoute(fastify);
 }
 
 function getStatusRoute(fastify: FastifyInstance) {
-  fastify.get('/status', async function handler(_request: FastifyRequest, _reply: FastifyReply) {
-    return await handleGetStatus(fastify.db);
-  });
+  fastify.get(
+    "/status",
+    async function handler(_request: FastifyRequest, _reply: FastifyReply) {
+      return await handleGetStatus(fastify.db);
+    }
+  );
 }
 
 async function handleGetStatus(db: Database) {
@@ -38,5 +43,5 @@ async function handleGetStatus(db: Database) {
   const query = sql`SELECT 1`;
   await db.execute(query);
 
-  return { status: 'OK' };
+  return { status: "OK" };
 }
