8e5057327bbdcd44cad6304380eaa68045111584
LucasLvy <lucaslevy10@gmail.com>
2024-04-19 11:40:41 +0200

    chore(backend): format

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,15 +1,15 @@
-import type { Database } from "@/db/drizzle";
-import { sql } from "drizzle-orm";
-import type { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import type { Database } from '@/db/drizzle';
+import { sql } from 'drizzle-orm';
+import type { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 
-import { getGenerateClaimLinkRoute } from "./generateClaimLink";
-import { getBalanceRoute } from "./getBalance";
-import { getCurrentExpenseRoute } from "./getCurrentExpense";
-import { getHistoricalBalanceRoute } from "./getHistoricalBalance";
-import { getOtp } from "./getOtp";
-import { getTransactionHistory } from "./getTransactionHistory";
-import { getRegisterRoute } from "./register";
-import { verifyOtp } from "./verifyOtp";
+import { getGenerateClaimLinkRoute } from './generateClaimLink';
+import { getBalanceRoute } from './getBalance';
+import { getCurrentExpenseRoute } from './getCurrentExpense';
+import { getHistoricalBalanceRoute } from './getHistoricalBalance';
+import { getOtp } from './getOtp';
+import { getTransactionHistory } from './getTransactionHistory';
+import { getRegisterRoute } from './register';
+import { verifyOtp } from './verifyOtp';
 
 export const addressRegex = /^0x0[0-9a-fA-F]{63}$/;
 
@@ -26,12 +26,9 @@ export function declareRoutes(fastify: FastifyInstance) {
 }
 
 function getStatusRoute(fastify: FastifyInstance) {
-  fastify.get(
-    "/status",
-    async function handler(_request: FastifyRequest, _reply: FastifyReply) {
-      return await handleGetStatus(fastify.db);
-    }
-  );
+  fastify.get('/status', async function handler(_request: FastifyRequest, _reply: FastifyReply) {
+    return await handleGetStatus(fastify.db);
+  });
 }
 
 async function handleGetStatus(db: Database) {
@@ -39,5 +36,5 @@ async function handleGetStatus(db: Database) {
   const query = sql`SELECT 1`;
   await db.execute(query);
 
-  return { status: "OK" };
+  return { status: 'OK' };
 }
