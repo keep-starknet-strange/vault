8bbbc900a2477a342cf85caa966d1d567d1df847
0xChqrles <clanier.dev@gmail.com>
2024-06-14 00:44:04 +0200

    update factory address

--- backend/src/constants/contracts.ts
+++ backend/src/constants/contracts.ts
@@ -1,4 +1,4 @@
-export const VAULT_FACTORY_ADDRESS = '0x060f7dc1dcb936fd9bff710d4a19da3870e611e014b3ceaa662d24ac87221894'
+export const VAULT_FACTORY_ADDRESS = '0x8dabd7ce1da04fada30dc43aa90dee1861d62f0575601b19feaf80f801938e'
 export const BLANK_ACCOUNT_CLASS_HASH = '0x022d51f548b95dda56852e1e3211ebdcc623637794baf768afff33807f8c4563'
 
 export enum Entrypoint {
