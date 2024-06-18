ce6069342c854abe8c50c98e48301cdcc55fc584
Lucas @ StarkWare <70894690+LucasLvy@users.noreply.github.com>
2024-06-18 15:01:22 +0200

    Lucas/historical tx (#89)
    * fix(backend): rework get_transaction_history endpoint
    
    * test(backend): fix execute from outside test

--- backend/test/getTransactionHistory.test.ts
+++ backend/test/getTransactionHistory.test.ts
@@ -5,30 +5,53 @@ import { afterAll, beforeAll, beforeEach, describe, expect, test } from 'vitest'
 import { buildApp } from '@/app'
 
 import * as schema from '../src/db/schema'
+const MOCK_TX_HASH = '0x71dc653ae79444baeca2f182154bbd0ebb2bff2d73a4cf4c13774b2c613e'
+const MOCK_TO_ADDRESS = '0x00057c4b510d66eb1188a7173f31cccee47b9736d40185da8144377b896d5ff3'
+const MOCK_USER = {
+  phone_number: '+33612345678',
+  nickname: 'papa johnny',
+  contract_address: MOCK_TO_ADDRESS,
+}
 
-function generateMockData(fromAddress: string, upperLimit: number, lowerLimit?: number, isResponseMock?: boolean) {
+function generateMockData(
+  fromAddress: string,
+  upperLimit: number,
+  lowerLimit?: number,
+  isResponseMock?: boolean,
+  user?: { phone_number: string; nickname: string; contract_address: string },
+) {
   const mockData = []
-  const numEntries = upperLimit
-  const startCnt = lowerLimit ? lowerLimit : 1
-  const mockTxnHash = '0x71dc653ae79444baeca2f182154bbd0ebb2bff2d73a4cf4c13774b2c613e17c'
-  const mockToAddrs = '0x00057c4b510d66eb1188a7173f31cccee47b9736d40185da8144377b896d5ff3'
+  const numEntries = (lowerLimit ?? 0) + upperLimit
+  const startCnt = lowerLimit ? lowerLimit + 1 : 1
 
   for (let i = startCnt; i <= numEntries; i++) {
-    const transferId = `${mockTxnHash}_${i}`
+    const transferId = `${MOCK_TX_HASH}_${i}`
     const network = 'starknet-mainnet'
-    const transactionHash = mockTxnHash
-    const toAddress = mockToAddrs
+    const transactionHash = MOCK_TX_HASH + String(i)
+    const toAddress = MOCK_TO_ADDRESS
     const amount = `0x${i.toString(16)}`
 
-    const blockTimestamp = null
+    const blockTimestamp = new Date(i)
     if (isResponseMock) {
       mockData.push({
-        network,
-        transactionHash,
-        fromAddress,
-        toAddress,
+        from:
+          fromAddress === MOCK_TO_ADDRESS
+            ? MOCK_USER
+            : (user as {
+                phone_number: string
+                nickname: string
+                contract_address: string
+              }),
+        to:
+          toAddress === MOCK_TO_ADDRESS
+            ? MOCK_USER
+            : (user as {
+                phone_number: string
+                nickname: string
+                contract_address: string
+              }),
         amount,
-        blockTimestamp,
+        transaction_timestamp: blockTimestamp.toISOString(),
       })
     } else {
       mockData.push({
@@ -52,8 +75,13 @@ describe('GET /transaction history route', () => {
   let container: StartedPostgreSqlContainer
   let app: FastifyInstance
   const testAddress = '0x004babd76a282efdd30b97c8a98b0f2e4ebb91e81b3542bfd124c086648a07af'
-  const invalidStarknetAddress = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
-  const invalidFromAddress = '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7'
+  const MOCK_USER_NAME = 'coco lasticot'
+  const MOCK_USER_PHONE = '+33698765432'
+  const test_user = {
+    phone_number: MOCK_USER_PHONE,
+    nickname: MOCK_USER_NAME,
+    contract_address: testAddress,
+  }
   beforeAll(async () => {
     container = await new PostgreSqlContainer().start()
     const connectionUri = container.getConnectionUri()
@@ -67,6 +95,7 @@ describe('GET /transaction history route', () => {
     })
 
     await app.ready()
+    await app.db.insert(schema.registration).values([MOCK_USER, test_user])
   })
 
   afterAll(async () => {
@@ -80,107 +109,73 @@ describe('GET /transaction history route', () => {
     await app.db.delete(schema.usdcTransfer)
   })
 
-  test('should return the 10 txns obj, total 10 entries, pagination 1', async () => {
+  test('should return the 9 txns obj, total 10 entries', async () => {
     await app.db.insert(schema.usdcTransfer).values(generateMockData(testAddress, 10))
-    const paginationStr = '1'
+    const startValue = 1
+    const first = 9
     const response = await app.inject({
       method: 'GET',
-      url: `/transaction_history?address=${testAddress}&pagination=${paginationStr}`,
+      url: `/transaction_history?address=${testAddress}&after=${startValue}&first=${first}`,
     })
 
     expect(response.statusCode).toBe(200)
-    expect(response.json()).toHaveProperty('page_count', 1)
 
-    const mockResponseObj = generateMockData(testAddress, 10, 1, true)
+    const mockResponseObj = generateMockData(testAddress, first, startValue, true, test_user)
     expect(response.json().transactions).toMatchObject(mockResponseObj)
-    expect(response.json().transactions).toHaveLength(10)
+    expect(response.json().transactions).toHaveLength(first)
   })
 
-  test('should return the default txns obj limit, total 9 entries, no pagination', async () => {
-    await app.db.insert(schema.usdcTransfer).values(generateMockData(testAddress, 9))
+  test('should return the first 9 entries', async () => {
+    await app.db.insert(schema.usdcTransfer).values(generateMockData(testAddress, 10))
+    const startValue = 0
+    const first = 9
     const response = await app.inject({
       method: 'GET',
-      url: `/transaction_history?address=${testAddress}`,
+      url: `/transaction_history?address=${testAddress}&first=${first}`,
     })
 
     expect(response.statusCode).toBe(200)
-    expect(response.json()).toHaveProperty('page_count', 1)
 
-    const mockResponseObj = generateMockData(testAddress, 9, 1, true)
+    const mockResponseObj = generateMockData(testAddress, first, startValue, true, test_user)
     expect(response.json().transactions).toMatchObject(mockResponseObj)
-    expect(response.json().transactions).toHaveLength(9)
+    expect(response.json().transactions).toHaveLength(first)
   })
-
-  test('should return the 1 to 10 txns obj, total 33 entries, pagination 1 default', async () => {
-    await app.db.insert(schema.usdcTransfer).values(generateMockData(testAddress, 33))
-    const paginationStr = '1'
+  test('should return all the txs', async () => {
+    const txsNb = 10
+    await app.db.insert(schema.usdcTransfer).values(generateMockData(testAddress, txsNb))
+    const first = 30
     const response = await app.inject({
       method: 'GET',
-      url: `/transaction_history?address=${testAddress}&pagination=${paginationStr}`,
+      url: `/transaction_history?address=${testAddress}&first=${first}`,
     })
 
     expect(response.statusCode).toBe(200)
-    expect(response.json()).toHaveProperty('page_count', 4)
 
-    const mockResponseObj = generateMockData(testAddress, 10, 1, true)
+    const mockResponseObj = generateMockData(testAddress, txsNb, 0, true, test_user)
     expect(response.json().transactions).toMatchObject(mockResponseObj)
-    expect(response.json().transactions).toHaveLength(10)
+    expect(response.json().transactions).toHaveLength(txsNb)
   })
 
-  test('should return the 11 to 20 txns obj, total 33 entries, pagination 2', async () => {
-    await app.db.insert(schema.usdcTransfer).values(generateMockData(testAddress, 33))
-    const paginationStr = '2'
+  test('should return empty list unknown address', async () => {
+    await app.db.insert(schema.usdcTransfer).values(generateMockData(testAddress, 10))
+    const first = 9
     const response = await app.inject({
       method: 'GET',
-      url: `/transaction_history?address=${testAddress}&pagination=${paginationStr}`,
+      url: `/transaction_history?address=0x0111111111111111111111111111111111111111111111111111111111111111&first=${first}`,
     })
 
     expect(response.statusCode).toBe(200)
-    expect(response.json()).toHaveProperty('page_count', 4)
 
-    const mockResponseObj = generateMockData(testAddress, 20, 11, true)
-    expect(response.json().transactions).toMatchObject(mockResponseObj)
-    expect(response.json().transactions).toHaveLength(10)
+    expect(response.json().transactions).toMatchObject([])
+    expect(response.json().transactions).toHaveLength(0)
   })
 
-  test('should return the 21 to 30 txns obj, total 33 entries, pagination 3', async () => {
-    await app.db.insert(schema.usdcTransfer).values(generateMockData(testAddress, 33))
-    const paginationStr = '3'
+  test('should return an error wrong address format', async () => {
+    await app.db.insert(schema.usdcTransfer).values(generateMockData(testAddress, 10))
+    const first = 9
     const response = await app.inject({
       method: 'GET',
-      url: `/transaction_history?address=${testAddress}&pagination=${paginationStr}`,
-    })
-
-    expect(response.statusCode).toBe(200)
-    expect(response.json()).toHaveProperty('page_count', 4)
-
-    const mockResponseObj = generateMockData(testAddress, 30, 21, true)
-    expect(response.json().transactions).toMatchObject(mockResponseObj)
-    expect(response.json().transactions).toHaveLength(10)
-  })
-
-  test('should return the 31 to 33 txns obj, total 33 entries, pagination 4', async () => {
-    await app.db.insert(schema.usdcTransfer).values(generateMockData(testAddress, 33))
-    const paginationStr = '4'
-    const response = await app.inject({
-      method: 'GET',
-      url: `/transaction_history?address=${testAddress}&pagination=${paginationStr}`,
-    })
-
-    expect(response.statusCode).toBe(200)
-    expect(response.json()).toHaveProperty('page_count', 4)
-
-    const mockResponseObj = generateMockData(testAddress, 33, 31, true)
-    expect(response.json().transactions).toMatchObject(mockResponseObj)
-    expect(response.json().transactions).toHaveLength(3)
-  })
-
-  test('should return error, invalid starknet address', async () => {
-    await app.db.insert(schema.usdcTransfer).values(generateMockData(testAddress, 5))
-    const paginationStr = '1'
-    const response = await app.inject({
-      method: 'GET',
-      url: `/transaction_history?address=${invalidStarknetAddress}&pagination=${paginationStr}`,
+      url: `/transaction_history?address=0x01&first=${first}`,
     })
 
     expect(response.statusCode).toBe(400)
@@ -195,30 +190,5 @@ describe('GET /transaction history route', () => {
     })
 
     expect(response.json()).toHaveProperty('error', 'Address is required.')
-  })
-
-  test('should return empty transactions arr and zero page cnt, invalid from address', async () => {
-    await app.db.insert(schema.usdcTransfer).values(generateMockData(testAddress, 5))
-    const paginationStr = '1'
-    const response = await app.inject({
-      method: 'GET',
-      url: `/transaction_history?address=${invalidFromAddress}&pagination=${paginationStr}`,
-    })
-
-    expect(response.statusCode).toBe(200)
-    expect(response.json()).toHaveProperty('page_count', 0)
-    expect(response.json()).toHaveProperty('transactions', [])
-  })
-
-  test('should return empty transactions arr and zero page cnt, valid from address, no db entries', async () => {
-    const paginationStr = '1'
-    const response = await app.inject({
-      method: 'GET',
-      url: `/transaction_history?address=${invalidFromAddress}&pagination=${paginationStr}`,
-    })
-
-    expect(response.statusCode).toBe(200)
-    expect(response.json()).toHaveProperty('page_count', 0)
-    expect(response.json()).toHaveProperty('transactions', [])
   })
 })
