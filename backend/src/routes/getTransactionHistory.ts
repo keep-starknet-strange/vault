d79109e2de53deb229458c9781dc21837336c7b7
0xChqrles <clanier.dev@gmail.com>
2024-06-27 17:43:41 +0200

    add `before` support for the txs pagination

--- backend/src/routes/getTransactionHistory.ts
+++ backend/src/routes/getTransactionHistory.ts
@@ -1,4 +1,5 @@
-import { and, desc, eq, lt, or, sql } from 'drizzle-orm'
+import { and, asc, desc, eq, gt, lt, or, sql } from 'drizzle-orm'
+import { PgSelectQueryBuilderBase } from 'drizzle-orm/pg-core'
 import type { FastifyInstance } from 'fastify'
 
 import { usdcTransfer } from '@/db/schema'
@@ -8,24 +9,45 @@ import { addressRegex } from '.'
 
 const MAX_PAGE_SIZE = 20
 
-function getCursorQuery(cursor?: string): Parameters<typeof and> {
+interface GetCursorQueryResult {
+  where: Parameters<typeof and>
+  order: Parameters<PgSelectQueryBuilderBase<any, any, any, any>['orderBy']>
+}
+
+function getCursorQuery(cursor: string | undefined, isReversed: boolean): GetCursorQueryResult {
   const [indexInBlock, timestamp] = fromCursorHash(cursor)
 
-  return [
-    or(
-      and(
-        timestamp ? eq(usdcTransfer.blockTimestamp, new Date(Number(timestamp) * 1000)) : undefined,
-        indexInBlock ? lt(usdcTransfer.indexInBlock, +indexInBlock) : undefined,
+  const sortingExpression = isReversed ? asc : desc
+  const compareExpression = isReversed ? gt : lt
+
+  return {
+    where: [
+      or(
+        and(
+          timestamp ? eq(usdcTransfer.blockTimestamp, new Date(Number(timestamp) * 1000)) : undefined,
+          indexInBlock ? compareExpression(usdcTransfer.indexInBlock, +indexInBlock) : undefined,
+        ),
+        timestamp ? compareExpression(usdcTransfer.blockTimestamp, new Date(Number(timestamp) * 1000)) : undefined,
       ),
-      timestamp ? lt(usdcTransfer.blockTimestamp, new Date(Number(timestamp) * 1000)) : undefined,
-    ),
-  ]
+    ],
+    order: [sortingExpression(usdcTransfer.blockTimestamp), sortingExpression(usdcTransfer.indexInBlock)],
+  }
+}
+
+interface CursorableTransaction {
+  index_in_block: number | null
+  transaction_timestamp: Date | null
+}
+
+function getCursor(tx: CursorableTransaction): string {
+  return toCursorHash((tx.index_in_block ?? 0).toString(), (tx.transaction_timestamp!.getTime() / 1000).toString())
 }
 
 interface TransactionHistoryQuery {
   address?: string
   first?: string
   after?: string
+  before?: string
 }
 
 export function getTransactionHistory(fastify: FastifyInstance) {
@@ -33,7 +55,7 @@ export function getTransactionHistory(fastify: FastifyInstance) {
     '/transaction_history',
 
     async (request, reply) => {
-      const { address, first: firstStr, after } = request.query as TransactionHistoryQuery
+      const { address, first: firstStr, after, before } = request.query as TransactionHistoryQuery
       const first = Number(firstStr ?? MAX_PAGE_SIZE)
 
       if (!address) {
@@ -44,12 +66,16 @@ export function getTransactionHistory(fastify: FastifyInstance) {
         return reply.status(400).send({ error: `First cannot exceed ${MAX_PAGE_SIZE}.` })
       }
 
+      if (after && before) {
+        return reply.status(400).send({ error: 'After and before are clashing' })
+      }
+
       // Validate address format
       if (!addressRegex.test(address)) {
         return reply.status(400).send({ error: 'Invalid address format.' })
       }
 
-      const afterQuery = getCursorQuery(after)
+      const paginationQuery = getCursorQuery(after ?? before, !!before)
 
       try {
         const txs = await fastify.db
@@ -73,20 +99,20 @@ export function getTransactionHistory(fastify: FastifyInstance) {
           .from(usdcTransfer)
           .leftJoin(sql`registration AS "from_user"`, eq(usdcTransfer.fromAddress, sql`"from_user"."contract_address"`))
           .leftJoin(sql`registration AS "to_user"`, eq(usdcTransfer.toAddress, sql`"to_user"."contract_address"`))
-          .where(and(...afterQuery, or(eq(usdcTransfer.fromAddress, address), eq(usdcTransfer.toAddress, address))))
+          .where(
+            and(
+              ...paginationQuery.where,
+              or(eq(usdcTransfer.fromAddress, address), eq(usdcTransfer.toAddress, address)),
+            ),
+          )
           .limit(Number(first) + 1)
-          .orderBy(desc(usdcTransfer.blockTimestamp), desc(usdcTransfer.indexInBlock))
+          .orderBy(...paginationQuery.order)
           .execute()
 
         // get pagination infos
         const lastTx = txs.length ? txs[Math.min(txs.length - 1, first - 1)] : null
 
-        const endCursor = lastTx
-          ? toCursorHash(
-              (lastTx.index_in_block ?? 0).toString(),
-              (lastTx.transaction_timestamp!.getTime() / 1000).toString(),
-            )
-          : null
+        const endCursor = lastTx ? getCursor(lastTx) : null
 
         const hasNext = txs.length > first
 
