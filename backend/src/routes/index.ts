14f536fb7a647fdaabcdc52781bc4e24c7b5f4b7
LucasLvy <lucaslevy10@gmail.com>
2024-06-28 18:29:14 +0200

    feat(backend): user from address

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -13,6 +13,7 @@ import { getHistoricalBalanceRoute } from './getHistoricalBalance'
 import { getLimitRoute } from './getLimit'
 import { getOtp } from './getOtp'
 import { getTransactionHistory } from './getTransactionHistory'
+import { getUserRoute } from './getUser'
 import { verifyOtp } from './verifyOtp'
 
 export const addressRegex = /^0x0[0-9a-fA-F]{63}$/
@@ -28,6 +29,7 @@ export function declareRoutes(fastify: FastifyInstance, deployer: Account, twili
   getClaimRoute(fastify)
   getLimitRoute(fastify)
   getExecuteFromOutsideRoute(fastify, deployer)
+  getUserRoute(fastify)
 }
 
 function getStatusRoute(fastify: FastifyInstance) {
